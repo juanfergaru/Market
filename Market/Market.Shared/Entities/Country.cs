cb5726f2b21d5f33fbf1b2ab3a9a919a9319d568
JUAN FERNANDO GALEANO RUIZ <juanfergaru182@gmail.com>
2023-08-16 19:48:18 -0500

    Entity countru

--- Market/Market.Shared/Entities/Country.cs
+++ Market/Market.Shared/Entities/Country.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Market.Shared.Entities
 {
-    internal class Country
+    public class Country
     {
-    }
+        public int Id { get; set; } 
+
+        [Display (Name ="País")]
+        [MaxLength (100)]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; } = null;
+
+     }
 }
